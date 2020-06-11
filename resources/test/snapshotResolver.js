8fdf9a83d58a834c5bd141d039f5105321d1f304
dschreij <dschreij@gmail.com>
2020-06-11 15:12:50 +0200

    Restore missing tests

--- resources/test/snapshotResolver.js
+++ resources/test/snapshotResolver.js
@@ -1,7 +1,9 @@
 module.exports = {
   testPathForConsistencyCheck: 'resources/pages/index.test.js',
   resolveSnapshotPath: (testPath, snapshotExtension) =>
-    testPath.replace(/\.test\.([tj]sx?)/, `${snapshotExtension}.$1`),
+    testPath
+      .replace(/\.test\.([tj]sx?)/, `${snapshotExtension}.$1`)
+      .replace(/resources([/\\]pages)/, '/resources/test/__snapshots__'),
   resolveTestPath: (snapshotFilePath, snapshotExtension) =>
     snapshotFilePath.replace(snapshotExtension, '.test')
 }
