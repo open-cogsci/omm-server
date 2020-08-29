6b67fd0d70d7beab82a7aa00810c118a01a6554d
Daniel Schreij <dschreij@gmail.com>
2020-08-29 21:11:18 +0200

    Updated release scripts

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,24 +4,20 @@ set -e
 
 : "${1:?}"
 
-releaseVersion=$(npm version "$1" -m "Release v%s")
+git checkout develop
+releaseVersion=$(npm version "$1" -m "Release v%s" --git-tag-version=false)
 
 echo
-echo ">>> Committed and tagged $releaseVersion"
-echo ">>> Will push $releaseVersion"
+echo ">>> Bumped to $releaseVersion"
 echo ">>> Will commit and push new snapshot version"
 echo
 read -p "OK? (Ctrl-C to cancel)" -n 1 -r && echo
 echo
 
+git flow release start "$releaseVersion"
+git flow release finish "$releaseVersion"
 git push
 git push --tags
-
-newVersion=$(npm version patch --git-tag-version=false)
-npm version "$newVersion-SNAPSHOT" --git-tag-version=false
-git add .
-git commit -m "Set snapshot version $newVersion-SNAPSHOT"
-git push
 
 echo
 echo ">>> Committed and pushed $releaseVersion"
