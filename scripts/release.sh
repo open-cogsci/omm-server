53ad41bc26ec35039e8fff08bef21caef4109af9
Daniel Schreij <dschreij@gmail.com>
2020-08-29 22:26:49 +0200

    Preparing vv0.1.12

--- scripts/release.sh
+++ scripts/release.sh
@@ -17,7 +17,7 @@ echo
 git commit -a -m "Preparing v$releaseVersion"
 git flow release start "$releaseVersion"
 git tag -a $releaseVersion -m $releaseVersion
-git flow release -m "release/$releaseVersion" -n finish $releaseVersion
+git flow release finish $releaseVersion -n -m "release/$releaseVersion"
 
 git push
 git push origin master
