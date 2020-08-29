618b99594924b23fd80be5e4489e09d95314ad2d
Daniel Schreij <dschreij@gmail.com>
2020-08-29 22:06:54 +0200

    Release script terror

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,17 +5,17 @@ set -e
 : "${1:?}"
 
 git checkout develop
-releaseVersion=$(npm version "$1" --git-tag-version=false)
+releaseVersion=$(npm version "$1" --dry-run)
 
 echo
-echo ">>> Bumped to $releaseVersion"
-echo ">>> Will commit and push new snapshot version"
+echo ">>> Releasing $releaseVersion"
+echo ">>> Will commit and push"
 echo
 read -p "OK? (Ctrl-C to cancel)" -n 1 -r && echo
 echo
 
-git commit -a -m "Preparing v$releaseVersion"
 git flow release start "$releaseVersion"
+npm version $releaseVersion
 git flow release finish -n
 
 git push
