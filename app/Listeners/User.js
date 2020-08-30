bfe53f4a4a65a6e9da01b3a9450cdc0c7db9cfde
dschreij <dschreij@gmail.com>
2020-08-30 11:28:19 +0200

    Graceful handling of email failure

--- app/Listeners/User.js
+++ app/Listeners/User.js
@@ -12,14 +12,18 @@ User.created = async ({ user, token }) => {
   const tokenString = querystring.encode({ token: pwtoken, etoken: token })
   const baseURL = Env.get('BASE_URL')
 
-  const mailer = await Mail.send('emails.newuser', { baseURL, user, tokenString }, (message) => {
-    message.to(user.email)
-    message.from(`<${Env.get('MAIL_USERNAME')}>`)
-    message.subject('Your account at OMM')
-  })
-  if (Env.get('MAIL_CONNECTION') === 'ethereal') {
-    // eslint-disable-next-line no-console
-    console.log(mailer)
+  try {
+    const mailer = await Mail.send('emails.newuser', { baseURL, user, tokenString }, (message) => {
+      message.to(user.email)
+      message.from(`<${Env.get('MAIL_USERNAME')}>`)
+      message.subject('Your account at OMM')
+    })
+    if (Env.get('MAIL_CONNECTION') === 'ethereal') {
+      // eslint-disable-next-line no-console
+      console.log(mailer)
+    }
+  } catch (e) {
+    console.error('Failed to send registration email', e)
   }
 }
 
@@ -27,26 +31,34 @@ User.emailChanged = async ({ user, token }) => {
   const tokenString = querystring.encode({ token })
   const baseURL = Env.get('BASE_URL')
 
-  const mailer = await Mail.send('emails.emailchanged', { baseURL, user, tokenString }, (message) => {
-    message.to(user.email)
-    message.from(`<${Env.get('MAIL_USERNAME')}>`)
-    message.subject('Please verify your e-mail address at OMM')
-  })
-  if (Env.get('MAIL_CONNECTION') === 'ethereal') {
-    // eslint-disable-next-line no-console
-    console.log(mailer)
+  try {
+    const mailer = await Mail.send('emails.emailchanged', { baseURL, user, tokenString }, (message) => {
+      message.to(user.email)
+      message.from(`<${Env.get('MAIL_USERNAME')}>`)
+      message.subject('Please verify your e-mail address at OMM')
+    })
+    if (Env.get('MAIL_CONNECTION') === 'ethereal') {
+      // eslint-disable-next-line no-console
+      console.log(mailer)
+    }
+  } catch (e) {
+    console.error('Failed to send email verification email', e)
   }
 }
 
 User.passwordChanged = async ({ user }) => {
-  const mailer = await Mail.send('emails.passwordchanged', { user }, (message) => {
-    message.to(user.email)
-    message.from(`<${Env.get('MAIL_USERNAME')}>`)
-    message.subject('Your OMM password was changed')
-  })
-  if (Env.get('MAIL_CONNECTION') === 'ethereal') {
+  try {
+    const mailer = await Mail.send('emails.passwordchanged', { user }, (message) => {
+      message.to(user.email)
+      message.from(`<${Env.get('MAIL_USERNAME')}>`)
+      message.subject('Your OMM password was changed')
+    })
+    if (Env.get('MAIL_CONNECTION') === 'ethereal') {
     // eslint-disable-next-line no-console
-    console.log(mailer)
+      console.log(mailer)
+    }
+  } catch (e) {
+    console.error('Failed to send password changed email', e)
   }
 }
 
@@ -54,13 +66,17 @@ User.forgotPassword = async ({ user, token }) => {
   const tokenString = querystring.encode({ token })
   const baseURL = Env.get('BASE_URL')
 
-  const mailer = await Mail.send('emails.resetPassword', { baseURL, user, tokenString }, (message) => {
-    message.to(user.email)
-    message.from(`<${Env.get('MAIL_USERNAME')}>`)
-    message.subject('Reset your password')
-  })
-  if (Env.get('MAIL_CONNECTION') === 'ethereal') {
+  try {
+    const mailer = await Mail.send('emails.resetPassword', { baseURL, user, tokenString }, (message) => {
+      message.to(user.email)
+      message.from(`<${Env.get('MAIL_USERNAME')}>`)
+      message.subject('Reset your password')
+    })
+    if (Env.get('MAIL_CONNECTION') === 'ethereal') {
     // eslint-disable-next-line no-console
-    console.log(mailer)
+      console.log(mailer)
+    }
+  } catch (e) {
+    console.error('Failed to send password reset email', e)
   }
 }
--- dockerfile
+++ dockerfile
@@ -4,15 +4,20 @@ RUN apt-get install python make g++ git -y
 WORKDIR /img
 COPY . .
 RUN npm clean-install
+ENV ENV_SILENT=true
+ENV APP_KEY=2wsjLSopTjD6WQEztTYIZgCFou8wpLJn
 # RUN npm test
 RUN npm run build
 RUN npm prune --production
 
 FROM node:14-alpine
 WORKDIR /img
+ENV ENV_SILENT=true
 ENV NODE_ENV=production
-ENV NUXT_HOST=0.0.0.0
-ENV NUXT_PORT=80
+ENV PORT=3000
+ENV BASE_URL=http://${HOST}:${PORT}
+ENV CACHE_VIEWS=false
+ENV APP_KEY=2wsjLSopTjD6WQEztTYIZgCFou8wpLJn
 COPY --from=builder ./img .
 CMD node ace migration:run --force && \
     node ace seed --files ProductionSeeder.js --force && \
