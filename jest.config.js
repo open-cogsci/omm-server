7c2b8cd14c8cd3d4cc15dc2fa55c978c7451658b
Daniel Schreij <dschreij@gmail.com>
2020-11-18 22:16:39 +0100

    Package updates and minor cleanup

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  // globalSetup: '<rootDir>/resources/test/jestGlobalSetup.js',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/resources/$1',
     '^~/(.*)$': '<rootDir>/resources/$1',
