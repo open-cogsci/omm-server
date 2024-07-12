8505b0acecdf3d5ad55f26216e3293dd1a3b1e85
kristian <lange.kristian@gmail.com>
2024-07-12 13:45:42 +0200

    database.js - switch to mysql as the default, use suitable default values and remove dysfunctional sqlite and postgres

--- config/database.js
+++ config/database.js
@@ -13,26 +13,7 @@ module.exports = {
   | interacting with SQL databases.
   |
   */
-  connection: Env.get('DB_CONNECTION', 'sqlite'),
-
-  /*
-  |--------------------------------------------------------------------------
-  | Sqlite
-  |--------------------------------------------------------------------------
-  |
-  | Sqlite is a flat file database and can be good choice under development
-  | environment.
-  |
-  | npm i --save sqlite3
-  |
-  */
-  sqlite: {
-    client: 'sqlite3',
-    connection: {
-      filename: Helpers.databasePath('development.sqlite')
-    },
-    useNullAsDefault: true
-  },
+  connection: Env.get('DB_CONNECTION', 'mysql'),
 
   /*
   |--------------------------------------------------------------------------
@@ -48,31 +29,11 @@ module.exports = {
     client: 'mysql2',
     connection: {
       host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      port: Env.get('DB_PORT', '3306'),
+      user: Env.get('DB_USER', 'ommuser'),
+      password: Env.get('DB_PASSWORD', 'abc123'),
+      database: Env.get('DB_DATABASE', 'omm')
     }
   },
 
-  /*
-  |--------------------------------------------------------------------------
-  | PostgreSQL
-  |--------------------------------------------------------------------------
-  |
-  | Here we define connection settings for PostgreSQL database.
-  |
-  | npm i --save pg
-  |
-  */
-  pg: {
-    client: 'pg',
-    connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
-    }
-  }
 }
