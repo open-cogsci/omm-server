74590f891c16f7021b8ef273f949f1ab93e152a1
kristian <lange.kristian@gmail.com>
2024-08-24 15:39:50 +0200

    Fix jest tests for study page incl participation panel and participation stats

---
+++ resources/test/__snapshots__/components/Participants/ParticipationStats/ParticipationStats.snap.js
@@ -0,0 +1,77 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`ParticipationStats matches its snapshot 1`] = `
+<div class="fill-height d-flex flex-column v-card v-sheet v-sheet--outlined theme--light">
+  <div class="v-card__title">
+    study_participants.stats.title
+  </div>
+  <div class="v-card__text">
+    <div class="row fill-height" id="participants-stats">
+      <div class="pb-5 col col-6">
+        <p class="text-subtitle-2 secondary--text">
+          stats.overview
+        </p>
+        <div class="row no-gutters font-weight-bold">
+          <div class="col-sm-7 col-lg-8 col-xl-6 col-6">
+            stats.participants.total:
+          </div>
+          <div class="col-sm-5 col-lg-4 col-xl-6 col-6">
+            0
+          </div>
+        </div>
+        <div class="row no-gutters">
+          <div class="col-sm-7 col-lg-8 col-xl-6 col-6">
+            - stats.participants.finished:
+          </div>
+          <div class="col-sm-5 col-lg-4 col-xl-6 col-6">
+            0
+          </div>
+        </div>
+        <div class="row no-gutters">
+          <div class="col-sm-7 col-lg-8 col-xl-6 col-6">
+            - stats.participants.in_progress:
+          </div>
+          <div class="col-sm-5 col-lg-4 col-xl-6 col-6">
+            0
+          </div>
+        </div>
+        <div class="row no-gutters">
+          <div class="col-sm-7 col-lg-8 col-xl-6 col-6">
+            - stats.participants.pending:
+          </div>
+          <div class="col-sm-5 col-lg-4 col-xl-6 col-6">
+            0
+          </div>
+        </div>
+      </div>
+      <div class="pb-5 col col-6">
+        <p class="text-subtitle-2 secondary--text">
+          stats.progress
+        </p>
+        <div aria-valuemax="100" aria-valuemin="0" aria-valuenow="0" class="v-progress-circular text-center v-progress-circular--visible green--text" role="progressbar" style="height: 175px; width: 175px;">
+          <svg style="transform: rotate(-90deg);" viewBox="23.333333333333332 23.333333333333332 46.666666666666664 46.666666666666664" xmlns="http://www.w3.org/2000/svg">
+            <circle class="v-progress-circular__underlay" cx="46.666666666666664" cy="46.666666666666664" fill="transparent" r="20" stroke-dasharray="125.664" stroke-dashoffset="0" stroke-width="6.666666666666666" />
+            <circle class="v-progress-circular__overlay" cx="46.666666666666664" cy="46.666666666666664" fill="transparent" r="20" stroke-dasharray="125.664" stroke-dashoffset="125.66370614359172px" stroke-width="6.666666666666666" />
+          </svg>
+          <div class="v-progress-circular__info">
+            <div>
+              <div class="text-h4 text-md-h2">
+                0
+                <span class="caption">%</span>
+              </div>
+              <div class="caption">
+                stats.complete
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col col-12">
+        <p class="text-subtitle-2 secondary--text">
+          stats.trend
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+`;
