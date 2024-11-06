26d59d5196dd6e8f20e401c1c202a842591f5525
kristian <lange.kristian@gmail.com>
2024-11-06 21:13:08 +0100

    Fix Jest test

--- resources/test/__snapshots__/pages/dashboard/index.snap.js
+++ resources/test/__snapshots__/pages/dashboard/index.snap.js
@@ -3,7 +3,7 @@
 exports[`Dashboard Index matches its snapshot 1`] = `
 <div class="container fill-height">
   <div class="row fill-height">
-    <div class="d-flex flex-column fill-height col col-12">
+    <div class="col col-12">
       <div class="row" style="max-height: 64px;">
         <div class="col col-12">
           <h1 class="display-1 font-weight-light">
@@ -11,49 +11,8 @@ exports[`Dashboard Index matches its snapshot 1`] = `
           </h1>
         </div>
       </div>
-      <div class="row fill-height">
-        <div class="col-sm-6 col-xl-4 col-12" style="height: 50%;">
-          <div class="fill-height v-card v-sheet theme--light" style="overflow: auto;">
-            <div class="v-card__title font-weight-normal">
-              dashboard.title.most_recent_ptcp
-            </div>
-            <div class="v-card__text px-0">
-              <div aria-busy="true" aria-live="polite" class="v-skeleton-loader v-skeleton-loader--is-loading theme--light" role="alert">
-                <div class="v-skeleton-loader__list-item-two-line v-skeleton-loader__bone">
-                  <div class="v-skeleton-loader__sentences v-skeleton-loader__bone">
-                    <div class="v-skeleton-loader__text v-skeleton-loader__bone"></div>
-                    <div class="v-skeleton-loader__text v-skeleton-loader__bone"></div>
-                  </div>
-                </div>
-                <div class="v-skeleton-loader__list-item-two-line v-skeleton-loader__bone">
-                  <div class="v-skeleton-loader__sentences v-skeleton-loader__bone">
-                    <div class="v-skeleton-loader__text v-skeleton-loader__bone"></div>
-                    <div class="v-skeleton-loader__text v-skeleton-loader__bone"></div>
-                  </div>
-                </div>
-                <div class="v-skeleton-loader__list-item-two-line v-skeleton-loader__bone">
-                  <div class="v-skeleton-loader__sentences v-skeleton-loader__bone">
-                    <div class="v-skeleton-loader__text v-skeleton-loader__bone"></div>
-                    <div class="v-skeleton-loader__text v-skeleton-loader__bone"></div>
-                  </div>
-                </div>
-                <div class="v-skeleton-loader__list-item-two-line v-skeleton-loader__bone">
-                  <div class="v-skeleton-loader__sentences v-skeleton-loader__bone">
-                    <div class="v-skeleton-loader__text v-skeleton-loader__bone"></div>
-                    <div class="v-skeleton-loader__text v-skeleton-loader__bone"></div>
-                  </div>
-                </div>
-                <div class="v-skeleton-loader__list-item-two-line v-skeleton-loader__bone">
-                  <div class="v-skeleton-loader__sentences v-skeleton-loader__bone">
-                    <div class="v-skeleton-loader__text v-skeleton-loader__bone"></div>
-                    <div class="v-skeleton-loader__text v-skeleton-loader__bone"></div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-6 col-xl-4 col-12" style="height: 50%;">
+      <div class="row">
+        <div class="col col-6" style="height: 50%;">
           <div class="fill-height v-card v-sheet theme--light">
             <div class="v-card__title">
               dashboard.title.most_active_studies&nbsp;
@@ -97,7 +56,7 @@ exports[`Dashboard Index matches its snapshot 1`] = `
             </div>
           </div>
         </div>
-        <div class="col-sm-6 col-xl-4 col-12" style="height: 50%;">
+        <div class="col col-6" style="height: 50%;">
           <div class="fill-height v-card v-sheet theme--light">
             <div class="v-card__title">
               dashboard.title.most_active_ptcp&nbsp;
@@ -141,27 +100,55 @@ exports[`Dashboard Index matches its snapshot 1`] = `
             </div>
           </div>
         </div>
-        <div class="col-sm-6 col-xl-12 col-12" style="height: 50%;">
-          <div class="fill-height d-flex flex-column v-card v-sheet theme--light">
-            <div class="v-card__title pb-0 mb-0">
-              <div>
-                dashboard.title.trend.main
+      </div>
+      <div class="row">
+        <div class="col col-12">
+          <div class="v-card v-sheet theme--light">
+            <div class="v-card__title d-flex">
+              <div class="me-auto">
+                Participation trend
                 <br>
                 <span class="font-weight-light text-subtitle-1">
-                  dashboard.title.trend.sub
+                  of all your current studies taken together
                 </span>
               </div>
-            </div>
-            <div class="v-card__text d-flex flex-column justify-center fill-height">
-              <div aria-busy="true" aria-live="polite" class="v-skeleton-loader v-skeleton-loader--is-loading theme--light" role="alert">
-                <transition-stub name="fade-transition">
-                  <div class="v-skeleton-loader__card v-skeleton-loader__bone">
-                    <div class="v-skeleton-loader__image v-skeleton-loader__bone"></div>
-                    <div class="v-skeleton-loader__card-heading v-skeleton-loader__bone">
-                      <div class="v-skeleton-loader__heading v-skeleton-loader__bone"></div>
+              <div>
+                <div class="v-input v-input--hide-details v-input--is-label-active v-input--is-dirty v-input--dense theme--light v-text-field v-text-field--enclosed v-text-field--outlined v-select">
+                  <div class="v-input__control">
+                    <div aria-expanded="false" aria-haspopup="listbox" aria-owns="list-10" class="v-input__slot" role="button">
+                      <fieldset aria-hidden="true">
+                        <legend style="width: 0px;">
+                          <span class="notranslate">​</span>
+                        </legend>
+                      </fieldset>
+                      <div class="v-select__slot">
+                        <label class="v-label v-label--active theme--light" for="input-10" style="left: 0px; position: absolute;">Select type</label>
+                        <div class="v-select__selections">
+                          <div class="v-select__selection v-select__selection--comma">Past week</div>
+                          <input aria-readonly="false" autocomplete="off" id="input-10" readonly="readonly" type="text">
+                        </div>
+                        <div class="v-input__append-inner">
+                          <div class="v-input__icon v-input__icon--append">
+                            <i aria-hidden="true" class="v-icon notranslate mdi mdi-menu-down theme--light"></i>
+                          </div>
+                        </div>
+                        <input type="hidden" value="week">
+                      </div>
+                      <div class="v-menu">
+                      </div>
                     </div>
                   </div>
-                </transition-stub>
+                </div>
+              </div>
+            </div>
+            <div class="v-card__text">
+              <div aria-busy="true" aria-live="polite" class="v-skeleton-loader v-skeleton-loader--is-loading theme--light" role="alert">
+                <div class="v-skeleton-loader__card v-skeleton-loader__bone">
+                  <div class="v-skeleton-loader__image v-skeleton-loader__bone"></div>
+                  <div class="v-skeleton-loader__card-heading v-skeleton-loader__bone">
+                    <div class="v-skeleton-loader__heading v-skeleton-loader__bone"></div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
