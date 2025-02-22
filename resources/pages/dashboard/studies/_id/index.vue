42f7084f004d8a273b487e51502bfb45ced29fb3
Kristian Lange <lange.kristian@gmail.com>
2025-02-22 18:38:22 +0100

    Study actions - add spinning wheel to 'DATA' button when downloading result data

--- resources/pages/dashboard/studies/_id/index.vue
+++ resources/pages/dashboard/studies/_id/index.vue
@@ -57,6 +57,7 @@
             <v-col class="flex-grow-1 flex-shrink-0 text-md-right">
               <study-actions
                 :loading="status.loading"
+                :downloading-result-data="status.downloadingResultData"
                 :study="study"
                 :jobs="jobs"
                 :user-can-edit="userCanEdit"
@@ -176,6 +177,7 @@ export default {
         savingStudyInfo: false,
         savingInfo: false,
         loading: false,
+        downloadingResultData: false,
         refreshingJobs: false
       },
       errors: {
@@ -471,9 +473,11 @@ export default {
       }
     },
     async downloadResultData () {
+      this.status.downloadingResultData = true
       await this.generateDataFile('csv')
       const url = this.dataFiles['data-csv'].path
       window.location.assign(url)
+      this.status.downloadingResultData = false
     }
   }
 }
